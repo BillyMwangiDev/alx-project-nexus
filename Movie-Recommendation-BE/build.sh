b85596522d71b2212dac227bf2241aca8b6f9f17
BillyMwangiDev <billymwangi200@gmail.com>
2026-02-07 08:34:34 +0300

    fix: Export env vars in build.sh

--- Movie-Recommendation-BE/build.sh
+++ Movie-Recommendation-BE/build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Export environment variables for build-time Django commands
+# These are needed because render.yaml env vars aren't available yet during build
+export DEBUG="${DEBUG:-True}"
+export ALLOWED_HOSTS="${ALLOWED_HOSTS:-*}"
+
 echo "Installing dependencies..."
 pip install --upgrade pip
 pip install -r requirements.txt
